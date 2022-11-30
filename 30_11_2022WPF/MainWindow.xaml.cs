eb9ffbd300d5d82996da7af444aa9a298470009b
NikitaB1221 <44338356+NikitaB1221@users.noreply.github.com>
2022-11-30 22:22:25 +0200

    Conteiners 2 + Calc(Beta)
    Another work with containers + not ready calculator

---
+++ 30_11_2022WPF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _30_11_2022WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Num_Click(object sender, RoutedEventArgs e)
+        {
+            var NumButton = (Button)sender;
+            if (tbnow.Text == "0")
+                tbnow.Text = $"{NumButton.Content}";
+            else
+                tbnow.Text += $"{NumButton.Content}";
+
+        }
+
+        private void SymLeft_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbnow.Text != "")
+                tbnow.Text = tbnow.Text.Remove(tbnow.Text.Length - 1);
+        }
+
+        private void SymC_Click(object sender, RoutedEventArgs e)
+        {
+            tbnow.Text = "0";
+            tbpast.Text = "0";
+        }
+
+        /*Class work */
+
+        //    private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        //    {
+        //        var rb = sender as RadioButton;
+        //        this.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString($"{rb.Content}"));
+        //    }
+    }
+
+}
